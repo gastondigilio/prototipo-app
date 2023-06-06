15841a24a4b5befe090eebe48ad4eb8c7646210f
Emanuel Germano <emanuelgermano90@gmail.com>
2023-06-06 13:32:29 -0300

    creacion de ruta getAllProducts

--- api/src/controllers/Products/getAllProducts.js
+++ api/src/controllers/Products/getAllProducts.js
@@ -1,30 +1,25 @@
 const { Productos, Categories } = require("../../db");
 
 async function getProducts(req, res, next) {
-    const { name } = req.query;
+    
     try {
-        const allProducts = async function getAllProducts() {
-            let products = await Productos.findAll({
-                attributes: ["id", "productName", "gender", "salePrice", "image1"],
-                include: [
-                    {
-                        model: Categories,
-                        attributes: ["name"],
-                    },
-                ],
-            });
-            return products;
-        };
-        if (name && allProducts) {
-            const products = allProducts.filter(product =>
-                product.productName.toLowerCase().includes(name.toLowerCase())
-            );
-            res.status(200).json(products);
-        } else if (allProducts) {
+
+        const allProducts = await Productos.findAll({
+            attributes: ["id", "productName", "gender", "salePrice", "image1"],
+            include: [
+                {
+                    model: Categories,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        
+        if (allProducts) {
             res.status(200).send(allProducts);
         } else {
             res.status(404).send("No products found");
         }
+
     } catch (error) {
         console.error(error);
     }
