fc8028ba5aae09d1e7911e9f169231087c143dba
Emanuel Germano <emanuelgermano90@gmail.com>
2023-06-13 16:58:50 -0300

    modificacion getcreate

--- api/src/controllers/ListPrice/postListPrice.js
+++ api/src/controllers/ListPrice/postListPrice.js
@@ -1,4 +1,5 @@
 const { ListPrice } = require("../../db");
+const { Op } = require('sequelize');
 
 async function postListPrice(req, res, next) {
 
@@ -8,7 +9,27 @@ async function postListPrice(req, res, next) {
 
         if (name && porcenList) {
 
-            const listPrice = await ListPrice.findOrCreate({ where: { name, porcenList } });
+            const listPrice = await ListPrice.findOrCreate({
+
+                where: {
+
+                    name: {
+
+                        [Op.eq]: name
+
+                    }
+
+                },
+
+                defaults: {
+
+                    name,
+
+                    porcenList
+                    
+                }
+
+            });
 
             res.status(201).send({ listPrice });
 
